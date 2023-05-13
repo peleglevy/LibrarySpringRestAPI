86ba82569ad0bd97b0ef5997cddd9fb69eb78372
peleglevy <mishlek3@gmail.com>
2023-05-13 19:33:08 +0300

    remove extension

--- src/test/java/com/home/libraryrestapi/LibraryrestapiApplicationTests.java
+++ src/test/java/com/home/libraryrestapi/LibraryrestapiApplicationTests.java
@@ -11,7 +11,6 @@ import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
